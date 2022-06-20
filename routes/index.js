1db07a1114c0b69ed1dc1f63bd073d4b85974fca
Viren Keswani <virenkeswani@gmail.com>
2022-06-20 21:48:24 +0530

    login and register working

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,81 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
-router.get('/',(req,res) => {
-    res.render('index.ejs' , {name : 'John'});
+const initializePassport = require('../passport-config');
+const { application } = require('express');
+initializePassport(
+    passport, 
+    email =>  users.find(user => user.email === email),
+    id =>  users.find(user => user.id === id)
+    )
+
+const users = [];
+
+router.get('/',checkAuthenticated , (req,res) => {
+    res.render('index.ejs' , {name : req.user.name});
 })
 
 
-router.get('/login',(req,res) => {
+router.get('/login',checkNotAuthenticated , (req,res) => {
     res.render('login.ejs', {
         layout: 'login.ejs'
     });
 })
 
-router.get('/register',(req,res) => {
+router.post('/login',checkNotAuthenticated ,passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+
+
+router.get('/register',checkNotAuthenticated ,(req,res) => {
     res.render('register.ejs', {
         layout: 'register.ejs'
     });
 })
 
+router.post('/register', checkNotAuthenticated ,async(req,res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password,10);
+        users.push({
+            id : Date.now().toString(),
+            name : req.body.name,
+            email : req.body.email,
+            password : hashedPassword
+        })
+        res.redirect('/login')
+    }
+    catch(err){
+        console.log(err)
+        res.redirect('/register')
+    }
+    console.log(users)
+    //res.send("works")
+})
+
+
+router.delete('/logout', (req,res) => {
+    req.logOut();
+    res.redirect('/login');
+}
+)
+
+
+function checkAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+function checkNotAuthenticated(req,res,next){
+    if(!req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/');
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,43 @@
+if(process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bcrypt = require('bcrypt');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
 
 
 
-const indexRouter = require('./routes/index.js')
+const passport = require('passport');
+
 
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
+//app.use(bcrypt)
 app.use(expressLayouts);
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+//use bcrypt
+
+const indexRouter = require('./routes/index.js');
+
+
+
+//take forms from the login.ejs and send them to the server.js
 
 
 app.use('/', indexRouter)
