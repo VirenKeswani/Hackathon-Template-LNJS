5992af2674657b7dfe7b557cfc464947cea402aa
Viren Keswani <virenkeswani@gmail.com>
2022-06-20 14:10:59 +0530

    login and register routes set without

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res) => {
-    res.send("hello World")
+    res.render('index.ejs' , {name : 'John'});
+})
+
+
+router.get('/login',(req,res) => {
+    res.render('login.ejs', {
+        layout: 'login.ejs'
+    });
+})
+
+router.get('/register',(req,res) => {
+    res.render('register.ejs', {
+        layout: 'register.ejs'
+    });
 })
 
 
